eed8a44a5aaf3acdcf8fd7c5a4db6f85e6e69edb
Sweta Shaw <swt.shw94@gmail.com>
2023-07-27 13:04:16 +0530

    generate session id based on NODE_ENV

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,11 +1,12 @@
-const MODE = 'DEV'
+const MODE = process.env.NODE_ENV
 const sessionId = 'test-session-id'
 
 export default function generateSessionId() {
-    if (MODE === 'DEV') {
-        return sessionId
-    }
-    else {
-        return 'session-' + Math.random().toString(36).substring(2, 15)
-    }
+	if(MODE === 'DEV') {
+		return sessionId
+	} else if(MODE === 'PROD') {
+		return 'session-' + Math.random().toString(36).substring(2, 15)
+	} else {
+		throw new Error('Unknown mode')
+	}
 }
